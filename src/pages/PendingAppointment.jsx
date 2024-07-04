b18b4e5d38698007ae6984fb4d83d7a1540de91c
DhruvSaboo <saboodhruv789@gmail.com>
2024-07-04 16:29:52 +0530

    pending_appt_api_integrated

--- src/pages/PendingAppointment.jsx
+++ src/pages/PendingAppointment.jsx
@@ -1,12 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTable } from "react-table";
 import "../styles/PendingAppointment.css";
-import data from "../../PendingAppointmentsData";
 import { IoIosArrowDown } from "react-icons/io";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_PENDING_APPOINTMENTS;
+const employee_name = import.meta.env.VITE_API_PENDING_APPOINTMENTS_EMPLOYEES;
+
 const PendingAppointment = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [employeeName, setEmployeeName] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        const data = response.data;
+        const [key, appointment] = Object.entries(data).find(
+          ([key]) => key !== "success"
+        );
+        const formattedAppointments = [
+          {
+            serviceName: appointment.services,
+            clientName: appointment.name,
+            contact: appointment.phone,
+            dateTime: `${appointment.date} - ${appointment.time}`,
+            workerName: appointment.prefEmployee,
+            duration: `${appointment.duration} min`,
+            assignEmployee: appointment.available_employees,
+            cancel: "Cancel",
+          },
+        ];
+        setAppointments(formattedAppointments);
+      })
+      .catch((error) => {
+        console.log("API fetching error");
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(employee_name)
+      .then((response) => {
+        setEmployeeName(response.data);
+      })
+      .catch((error) => {
+        console.log("employee_name fetching error");
+        console.error(error);
+      });
+  }, []);
+
+  // const handleAssignChange = (e, appointment) => {
+  //   const selectedEmployee = e.target.value;
+  //   // console.log(
+  //   //   `Assigned employee ${selectedEmployee} to appointment ${appointment.clientName}`
+  //   // );
+  // };
+
   const columns = React.useMemo(
     () => [
       {
@@ -15,7 +67,9 @@ const PendingAppointment = () => {
         Cell: ({ value }) => (
           <div className="pending-appt-service-list">
             {value.map((service, index) => (
-              <span key={index} className="pending-appt-service-item">{service}</span>
+              <span key={index} className="pending-appt-service-item">
+                {service}
+              </span>
             ))}
           </div>
         ),
@@ -35,6 +89,7 @@ const PendingAppointment = () => {
       {
         Header: "Preferred Worker",
         accessor: "workerName",
+        Cell: ({ value }) => <span>{employeeName[value]}</span>,
       },
       {
         Header: "Duration",
@@ -43,10 +98,20 @@ const PendingAppointment = () => {
       {
         Header: "Assign Employee",
         accessor: "assignEmployee",
-        Cell: () => (
-          <button className="pending-appt-assign-btn">
-            Assign <IoIosArrowDown />
-          </button>
+        Cell: ({ value, row }) => (
+          <select
+            className="pending-appt-assign-select"
+            // onChange={(e) => handleAssignChange(e, row.original)}
+          >
+            <option value = "">Assign</option>
+            {Array.isArray(value)
+              ? value.map((employee, index) => (
+                  <option key={index} value={employee}>
+                    {employeeName[employee]}
+                  </option>
+                ))
+              : null}
+          </select>
         ),
       },
       {
@@ -55,21 +120,23 @@ const PendingAppointment = () => {
         Cell: () => <button className="pending-appt-cancel-btn">Cancel</button>,
       },
     ],
-    []
+    [employeeName]
   );
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({
       columns,
-      data,
+      data: appointments,
     });
 
   return (
     <div className="pending-appointments-container">
       <div className="pending-appointments-header">
         <h2>Pending Appointments</h2>
-        <NavLink to = "/AppointmentForm" className="navlink">
-          <button className="pending-add-appointment-btn">Add a new Appointment</button>
+        <NavLink to="/AppointmentForm" className="navlink">
+          <button className="pending-add-appointment-btn">
+            Add a new Appointment
+          </button>
         </NavLink>
       </div>
       <table {...getTableProps()} className="pending-appointments-table">
